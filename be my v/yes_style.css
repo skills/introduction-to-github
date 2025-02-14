fe9afdad19060982977c75869f7f96e51c852655
KageOutlook <aungoaggar@gmail.com>
2025-02-14 11:23:45 +0630

    yes_style.css

---
+++ be my v/yes_style.css
@@ -0,0 +1,24 @@
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #f9e3e3;
+    font-family: 'Arial', sans-serif;
+}
+
+.container {
+    text-align: center;
+}
+
+.header_text {
+    font-size: 3em;
+    color: #d32f2f;
+}
+
+.gif_container img {
+    width: 100%; 
+    max-width: 500px; 
+    height: auto; 
+}
