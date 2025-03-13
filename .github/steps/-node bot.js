78a5eebe7c3e693160ef07184af2de6e7888ef13
soufianemeziany13 <soufianemeziany13@gmail.com>
2025-03-13 05:34:47 +0000

    Update and rename -step.txt to -node bot.js
    BOT WhatsApp

---
+++ .github/steps/-node bot.js
@@ -0,0 +1,23 @@
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    if (message.body === '!ping') {
+        message.reply('pong');
+    }
+});
+
+client.initialize();
