8ab1a18598c0eda5853ca73140f4fabd2e2fde42
Ksenia46-wp <kseniaborodina.728@gmail.com>
2025-03-16 21:24:47 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('привет')
+})
