24714e2cc8d12c3f7cdb800c5f915017397200d4
codefrog818 <cbao.uk@gmail.com>
2025-04-05 11:42:28 +0100

    test

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello world!!")
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Add a StreamHandler to output logs to the console
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+logger.info("hello world!!!!")
+logger.info("this is the test for my first branch!")
+
+print('a')
